23aee4f639b6f1db43bfd48d97175ca7f1b501f9
TempestShaw <109138344+TempestShaw@users.noreply.github.com>
2023-08-30 21:28:56 +0800

    Delete cache

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-from flask_caching import Cache
 from function import *
 from api import api_blueprint
 import os
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.config['SECRET_KEY'] = os.urandom(24)  # set key for session
 app.register_blueprint(api_blueprint)
 df_ttype = DataInit()
